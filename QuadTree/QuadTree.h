c6a5c458d0c4df1e3a8de16e8bcb6152ed2896a4
hershalle <hershalle@gmail.com>
2017-12-04 16:26:51 +0200

    Initial Commit

---
+++ QuadTree/QuadTree.h
@@ -0,0 +1,19 @@
+//
+//  QuadTree.h
+//  QuadTree
+//
+//  Created by Shai Balassiano on 04/12/2017.
+//  Copyright © 2017 hershalle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QuadTree.
+FOUNDATION_EXPORT double QuadTreeVersionNumber;
+
+//! Project version string for QuadTree.
+FOUNDATION_EXPORT const unsigned char QuadTreeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QuadTree/PublicHeader.h>
+
+
